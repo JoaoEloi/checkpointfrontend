6f513c76fcd5f49df2fb41118d1d66563a45720a
JoaoEloi <ejoao605@gmail.com>
2022-03-20 22:57:09 -0300

    Apenas sofrimento com essa porcaria

---
+++ src/componentes/header.jsx
@@ -0,0 +1,13 @@
+import Logo from './my-portfolio-imagem.png'
+
+
+function Header() {
+    return(
+        <>
+            <img src={Logo} id="logo" alt="Logo do site"/>
+            
+        </>
+    )
+}
+
+export default Header;
