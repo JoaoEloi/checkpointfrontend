431b92d1cfa3ecf5e320eeb94aa47b12a0c78edd
JoaoEloi <ejoao605@gmail.com>
2022-04-27 02:37:27 -0300

    outros ajustes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,31 +1,50 @@
-import './header.css'
-import Logo from '../../assets/logo.png'
-import { Button } from 'reactstrap';
+import "./header.css";
+import Logo from "../../assets/logo.png";
+import { Button } from "reactstrap";
 
-export default function Header () {
-    
-    function scrollToComponent(id) {
-        document.getElementById(id).scrollIntoView()
-    }
+export default function Header() {
+  function scrollToComponent(id) {
+    document.getElementById(id).scrollIntoView();
+  }
 
-    return (
+  return (
+    <nav className="nav_container_home">
+      <img src={Logo} alt="Logo do site" className="logo_header_home" />
 
+      <ul className="container_buttons">
+        <li>
+          {" "}
+          <Button
+            color="dark"
+            onClick={() => scrollToComponent("banner")}
+            className="header_buttons_item"
+          >
+            Home
+          </Button>{" "}
+        </li>
 
-        <header className="header_container_home">
-    
-            <img src={Logo} alt="Logo do site" className="logo_header_home" />
+        <li>
+          {" "}
+          <Button
+            color="dark"
+            onClick={() => scrollToComponent("body")}
+            className="header_buttons_item"
+          >
+            Portifolio
+          </Button>{" "}
+        </li>
 
-        <ul className="container_buttons">
-
-            <li> <Button color="dark" onClick={() => scrollToComponent("banner")} className="header_buttons_item">Home</Button> </li>
-
-            <li> <Button color="dark"  onClick={() => scrollToComponent("body")} className="header_buttons_item">Portifolio</Button> </li>
-
-            <li> <Button color="dark" onClick={() => scrollToComponent("footer")} className="header_buttons_item">Contatos</Button> </li>
-
-        </ul>    
-        </header>
-        
-    );
-};
-
+        <li>
+          {" "}
+          <Button
+            color="dark"
+            onClick={() => scrollToComponent("footer")}
+            className="header_buttons_item"
+          >
+            Contatos
+          </Button>{" "}
+        </li>
+      </ul>
+    </nav>
+  );
+}
